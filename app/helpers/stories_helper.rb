043ab78edca7efeb50593ba9ac0877e3df83592e
Andrzej Trzaska <atrzaska2@gmail.com>
2014-07-29 11:56:04 +0200

    small fix, add turbolinks id containter

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,35 +1,34 @@
 module StoriesHelper
   def story_actions(story)
-    next_links = case story.state
-       when 'unstarted'
-         [{ status: 'started', color: :default }]
-       when 'started'
-         [{ status: 'finished', color: :primary }]
-       when 'finished'
-         [{ status: 'delivered', color: :primary }]
-       when 'delivered'
-         [{ status: 'accepted', color: :primary }, { status: 'Rejected', color: :primary }]
-       when 'rejected'
-         [{ status: 'unstarted', color: :primary }]
+    links = case story.state
+              when 'unstarted'
+               [{ status: 'started', color: :default }]
+              when 'started'
+               [{ status: 'finished', color: :primary }]
+              when 'finished'
+               [{ status: 'delivered', color: :warning }]
+              when 'delivered'
+               [{ status: 'accepted', color: :success }, { status: 'rejected', color: :danger }]
+              when 'rejected'
+               [{ status: 'unstarted', color: :default }]
+              else []
      end
 
-    links = []
+    out = []
 
-    next_links.each_with_object(links) do |link, obj|
+    links.each_with_object(out) do |link, obj|
       obj << link_to(link[:status], project_story_path(story.project, story, story: { state: link[:status]}), method: :put, class: "btn btn-#{link[:color]}")
     end
 
-    links << link_to('x', project_story_path(story.project, story), method: :delete, class: 'btn btn-danger', data: { confirm: "Are you sure to delete #{story.name}?" })
-    links.join('').html_safe
+    out << link_to('x', project_story_path(story.project, story), method: :delete, class: 'btn btn-danger', data: { confirm: "Are you sure to delete #{story.name}?" })
+    out.join('').html_safe
   end
 
   def get_registered_members(project)
     members = []
 
     project.members.each do |member|
-      if member.user.present?
-        members << member
-      end
+      members << member if member.user.present?
     end
 
     members
